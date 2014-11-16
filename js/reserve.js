dbf92ddb39d359b7b625e6c83025020bd04ce919
Garrett <emrickgarrett@gmail.com>
2014-11-16 07:05:24 -0500

    Added in another type of room here at UC

--- js/reserve.js
+++ js/reserve.js
@@ -9,6 +9,308 @@ function getRoomData(search){
 
     search = search.toLowerCase();
     switch(search){
+        case "801d project room":
+        case "801e project room":
+            return {
+                chairs:[
+                    {
+                        "table_id" : 1,
+                        "x" : 100,
+                        "y" : 160,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 1,
+                        "x" : 200,
+                        "y" : 160,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 1,
+                        "x" : 280,
+                        "y" : 80,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 2,
+                        "x" : 480,
+                        "y" : 160,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 2,
+                        "x" : 580,
+                        "y" : 160,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 2,
+                        "x" : 400,
+                        "y" : 80,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 3,
+                        "x" : 500,
+                        "y" : 320,
+                        "status" : "handicap"
+                    },
+                    {
+                        "table_id" : 3,
+                        "x" : 580,
+                        "y" : 400,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 4,
+                        "x" : 300,
+                        "y" : 400,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 4,
+                        "x" : 300,
+                        "y" : 250,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 7,
+                        "x" : 580,
+                        "y" : 470,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 6,
+                        "x" : 480,
+                        "y" : 470,
+                        "status" : open
+                    },
+                    {
+                        "table_id" : 5,
+                        "x" : 380,
+                        "y" : 470,
+                        "status" : "handicap"
+                    }
+
+                ],
+                tables :[
+                    {
+                        "id": 1,
+                        "x": 60,
+                        "y": 45,
+                        status: "open"
+                    },
+                    {
+                        "id" : 1,
+                        "x" : 160,
+                        "y" : 45,
+                        status : "open"
+                    },
+                    {
+                        "id": 2,
+                        "x": 440,
+                        "y": 45,
+                        status: "taken"
+                    },
+                    {
+                        "id" : 2,
+                        "x" : 540,
+                        "y" : 45,
+                        status : "taken"
+                    },
+                    {
+                        "id" : 3,
+                        "x" : 540,
+                        "y" : 285,
+                        status : "open"
+                    },
+                    {
+                        "id" : 4,
+                        "x" : 260,
+                        "y" : 285,
+                        status : "taken"
+                    },
+                    {
+                        "id": 5,
+                        "x": 340,
+                        "y": 505,
+                        status: "open"
+                    },
+                    {
+                        "id": 6,
+                        "x": 440,
+                        "y": 505,
+                        status: "taken"
+                    },
+                    {
+                        "id" : 7,
+                        "x" : 540,
+                        "y" : 505,
+                        status : "open"
+                    }
+                ],
+                walls:[
+                    {
+                        "x" :10,
+                        "y" :20,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" :10,
+                        "y" :100,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" :10,
+                        "y" :180,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" :10,
+                        "y" :260,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" :10,
+                        "y" :340,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" :10,
+                        "y" :420,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" :10,
+                        "y" :500,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 10,
+                        "y" : 560,
+                        "rotate" : "yes"
+                    },//Left Wall
+                    {
+                        "x" : 10,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 90,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 170,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 250,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 330,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 410,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 490,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 560,
+                        "y" : 0,
+                        "rotate" : "no"
+                    },//Top Wall
+                    {
+                        "x" : 640,
+                        "y" : 0,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 640,
+                        "y" : 80,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 640,
+                        "y" : 160,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 640,
+                        "y" : 240,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 640,
+                        "y" : 320,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 640,
+                        "y" : 400,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 640,
+                        "y" : 480,
+                        "rotate" : "yes"
+                    },
+                    {
+                        "x" : 640,
+                        "y" : 560,
+                        "rotate" : "yes"
+                    },//Right Wall
+                    {
+                        "x" : 590,
+                        "y" : 610,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 510,
+                        "y" : 610,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 430,
+                        "y" : 610,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 350,
+                        "y" : 610,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 270,
+                        "y" : 610,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 190,
+                        "y" : 610,
+                        "rotate" : "no"
+                    },
+                    {
+                        "x" : 10,
+                        "y" : 610,
+                        "rotate" : "no"
+                    }
+                ],
+                plants:[
+
+                ]
+            };
+
         case "801c project room":
         case "801b project room":
         case "801a project room":
