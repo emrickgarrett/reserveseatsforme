51d0c599a089c8f80621ab8640a82796d7eb99b0
Garrett <emrickgarrett@gmail.com>
2014-11-16 07:47:53 -0500

    Added information about future possibilities for the software on the learnMore page

--- learnMore.php
+++ learnMore.php
@@ -12,31 +12,43 @@ require("dry/header.php");
 
 
 <!-- content -->
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
+<div class="container">
+    <div class="jumbotron">
+        <h1>The Future of ReserveSeatsFor.Me</h1>
+        <p class="lead">What plans I have for the site, improvements, and what can be possible with this tool...</p>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-4 box">
+            <h3 style="text-decoration:underline;">Events</h3>
+            <ul>
+               <li><p style="text-align:left;">Having the ability to plan events, and invite users through Gmail, Facebook, and Twitter...</p></li>
+               <li><p style="text-align:left;">Plan Study groups at the Library, Seats at the theatre, Workspace at a Hackathon...</p></li>
+                <li><p style="text-align:left;">Eliminate waiting time at restaurants and events, view seating in real time...</p></li>
+            </ul>
+        </div>
+
+        <div class="col-lg-4 box">
+            <h3 style="text-decoration:underline;">Improved Tools</h3>
+            <ul>
+                <li><p style="text-align:left;">Better customization. Multiple seat styles, adding logos, creating architecture...</p></li>
+                <li><p style="text-align:left;">Upload a REAL floorplan and have it converted to our format...</p></li>
+                <li><p style="text-align:left;">Being able to reserve and see reservations scheduled at any time in the future...</p></li>
+            </ul>
+        </div>
+
+        <div class="col-lg-4 box">
+            <h3 style="text-decoration:underline;">Additional Features</h3>
+            <ul>
+                <li><p style="text-align:left;">Links to tickets or coupons for Restaurants and events...</p></li>
+                <li><p style="text-align:left;">Sharing plans with friends, and having it update your calendar...</p></li>
+                <li><p style="text-align:left;">And Much, much more...</p></li>
+            </ul>
+        </div>
+    </div>
+
+
+</div>
 
 
 <?php
